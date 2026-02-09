e0db2092fd1940504a8dd53fbbf45737e01e001e
Shubham Shashikant Bendre <168751308+ShubhamB0107@users.noreply.github.com>
2026-02-09 16:18:28 +0000

    Created dataTypes file

---
+++ 01-Basics/01_dataTypes.js
@@ -0,0 +1,10 @@
+// alert(3+3)  //WE are using modejs, not browser
+
+let name = "Shubham" //string
+let age = 22 //number  & bigint for big numbers
+let isLoggeIn = false //boolean  true/false
+let num = null //null value
+//undefined => value not defined
+//symbol=> unique
+
+console.log(typeof"Shubham");
